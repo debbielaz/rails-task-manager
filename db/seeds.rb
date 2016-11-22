e1c1cfddd4ee6491b9953ada612c58fd167c86cc
debora lasoen <dlasoen@gmail.com>
2016-11-22 17:14:35 +0100

    tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(name:"Shopping")
+Task.create(name:"Sport")
+Task.create(name:"Cooking")
+Task.create(name:"Christmas cards")
